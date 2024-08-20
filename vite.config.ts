07d2598dc81660420ad1496fdfe9c1cee2310265
Bruno Carnavalli <carnavallibruno@gmail.com>
2024-08-20 18:19:37 -0300

    feat: Add basic configs to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: 'build',
+  },
+  base: '/',
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    css: true,
+    setupFiles: 'src/setupTests.ts',
+    includeSource: ['src/components/**/*.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportOnFailure: true,
+      include: ['src/components/**/*.{ts,tsx}'],
+      exclude: [
+        'node_modules/',
+        'src/components/index.ts',
+        'src/setupTests.ts',
+        'src/components/**/*.stories.{ts,tsx}'
+      ],
+    },
+  },
+  define: {
+    'import.meta.vitest': 'undefined',
+  }
 })
